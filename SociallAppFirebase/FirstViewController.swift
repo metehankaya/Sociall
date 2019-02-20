4c4c8ccda88e646222dcf2059d5dfb047f0dab48
Metehan Kaya <metehankaya25@gmail.com>
2019-02-20 23:54:12 +0300

    Initial Commit

---
+++ SociallAppFirebase/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  SociallAppFirebase
+//
+//  Created by Metehan Kaya on 20.02.2019.
+//  Copyright © 2019 Metehan Kaya. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SociallAppFirebase/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  SociallAppFirebase
+//
+//  Created by Metehan Kaya on 20.02.2019.
+//  Copyright © 2019 Metehan Kaya. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
